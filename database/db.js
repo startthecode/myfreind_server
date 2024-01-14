fe6687ae1ac3cde4d0739a169eda8009f2754521
Ashish Gola - Personal <ashugola1122000@gmail.com>
2024-01-14 16:22:03 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -1,17 +1,19 @@
+import "dotenv/config";
 import { Sequelize } from "sequelize";
 
 export let db_Credentials = {
-  user: "root",
-  password: "ashugola",
-  database: "myfriend",
+  user: process.env.SQL_USER_NAME,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE_NAME,
   host: "localhost",
 };
 
-
-export const sequelize = new Sequelize('myfriend', 'root', 'ashugola', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
-
-
-
+export const sequelize = new Sequelize(
+  process.env.SQL_DATABASE_NAME,
+  process.env.SQL_USER_NAME,
+  process.env.SQL_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
