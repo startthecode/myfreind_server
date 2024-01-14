7924f15534ff709f37a4167f82bce2478d9f0793
Ashish Gola - Personal <ashugola1122000@gmail.com>
2024-01-14 17:40:39 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ export let db_Credentials = {
   user: process.env.SQL_USER_NAME,
   password: process.env.SQL_PASSWORD,
   database: process.env.SQL_DATABASE_NAME,
-  host: "localhost",
+  host: process.env.SQL_HOST,
 };
 
 export const sequelize = new Sequelize(
@@ -13,7 +13,7 @@ export const sequelize = new Sequelize(
   process.env.SQL_USER_NAME,
   process.env.SQL_PASSWORD,
   {
-    host: "localhost",
+    host: process.env.SQL_HOST,
     dialect: "mysql",
   }
 );
